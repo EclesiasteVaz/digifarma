3dbbf79ad010481575e7a037c2eae69e25304818
EclesiasteVaz <121047599+EclesiasteVaz@users.noreply.github.com>
2023-11-02 11:03:55 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once "./api/config/sessao-iniciada.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-ao">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DigiFarma</title>
+    <link rel="stylesheet" href="style/body.css">
+    <link rel="stylesheet" href="style/div-up.css">
+    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="style/icons.css">
+</head>
+<body>
+    <div class="up">
+        <h1 class="icon-site"></h1>
+        <span class="icon-progress"></span>
+    </div>
+    <a id="link-login" href="login/"></a>
+    <script src="js/main-index.js" defer></script>
+</body>
+</html>
